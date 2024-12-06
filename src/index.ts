8e46ff2090fcca8f1248f45662085f65ed4f043b
steven <steven.nevins.js@gmail.com>
2024-12-06 14:38:30 -0500

    chore: prepare package

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { andThen } from './andThen'
+export { attempt } from './attempt'
+export { map, mapError } from './map'
+export { match } from './match'
+export { pipe } from './pipe'
+export { tap } from './tap'
+
+export type { Result, Success, Failure, Either } from './types'
